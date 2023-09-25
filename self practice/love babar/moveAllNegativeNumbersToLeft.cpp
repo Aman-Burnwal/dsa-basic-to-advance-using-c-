57bb762faad259ac8c8715f7517d5c1c8ab2ec96
Aman burnwal <94777700+Aman-Burnwal@users.noreply.github.com>
2023-09-25 15:21:19 +0530

    not completed

---
+++ self practice/love babar/moveAllNegativeNumbersToLeft.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {8, 1, - 1, - 4, 3, 2, 1};
+
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+
+    for(int i = 0; i < n; i++) {
+
+        if()
+
+    }
+
+
+
+    return 0;
+}
